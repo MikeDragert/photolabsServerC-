2425800f780792e6b3d41a6820cf1968c3abde4c
Mike Dragert <mikedragert@gmail.com>
2024-05-10 13:36:27 -0400

    Add project files.

---
+++ photolabsServerC#/DAL/PhotolabContext.cs
@@ -0,0 +1,21 @@
+﻿
+using Photolabs.Models;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace photolabsServerC_.DAL
+{
+    public class PhotolabContext : DbContext
+    {
+        public PhotolabContext() : base("PhotolabsContext") {
+        }
+
+        public DbSet<UserAccount> UserAccounts { get; set; }
+        public DbSet<Topic> Topics { get; set; }
+        public DbSet<Photo> Photos { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder) {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
